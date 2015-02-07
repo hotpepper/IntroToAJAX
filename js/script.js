4a9d7c14290b2964e9a635b71e2eec701eec51bb
hotpepper <heyseth@hotmail.com>
2015-02-07 16:43:51 -0200

    updates ignore

--- js/script.js
+++ js/script.js
@@ -20,15 +20,15 @@ function loadData() {
     // YOUR CODE GOES HERE!
     var street = $street.text("").val();
     var city = $city.text("").val();
-    console.log(street)
-    var city 
+    console.log(street);
+    var city ;
     if ($street.text("").val() =="") {
         var imgParam = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76"
     }
     else {
         var imgParam = imgBase  + street  + "," + city
         $greeting.text("So you want to live at " + street +"? Really, i heard it was a shit hole...")
-    }
+    };
     
     //$("body").append("<img src = https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10>")
     
