2986cde881388b307c2305daf0ab8e81280283f9
hotpepper <heyseth@hotmail.com>
2015-02-07 18:41:10 -0200

    adds nyt ajax request

--- js/script.js
+++ js/script.js
@@ -20,8 +20,9 @@ function loadData() {
     // YOUR CODE GOES HERE!
     var street = $street.text("").val();
     var city = $city.text("").val();
-    console.log(street);
-    var city ;
+    //console.log(street);
+    
+    //set default view
     if ($street.text("").val() =="") {
         var imgParam = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76"
     }
@@ -29,11 +30,21 @@ function loadData() {
         var imgParam = imgBase  + street  + "," + city
         $greeting.text("So you want to live at " + street +"? Really, i heard it was a shit hole...")
     };
-    
-    //$("body").append("<img src = https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10>")
-    
     $body.append('<img class="bgimg" src="'+imgParam+'">');
 
+    //NYT AJAX request
+    var key =  // key goes here
+    var URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?callback=svc_search_v2_articlesearch&q="+city+"&sort=newest&api-key="+key
+    $.getJSON(URL, function(data){
+        var items = [];
+        for(var i = 0; i < data.response.docs.length; i++) {
+            var article = data.response.docs[i];
+            console.log(article)
+             $("#nytimes-articles").append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>')
+        }
+        
+        console.log(data);
+    });
     return false;
 };
 
